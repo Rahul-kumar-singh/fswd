e4a48f75d9e3a39fdf3aaee7ad92e8cf4b9a6ff7
Rahul-kumar-singh <resoning123@gmail.com>
2021-07-14 12:32:57 -0700

    git commit

---
+++ js/assignment/second.js
@@ -0,0 +1,8 @@
+//	Write a program to print 10 to 1 number using a whileloop.
+
+let x = 10;
+while(x >= 1)
+{
+    console.log(x);
+    x--;
+}
---
+++ js/assignment/third.js
@@ -0,0 +1,8 @@
+// Write a program to print 5 to 15 numbers using For Loop
+
+let x = 5;
+while(x <= 15)
+{
+    console(x);
+    x++;
+}
